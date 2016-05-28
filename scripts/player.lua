50c490c82092ecf8c834849ce6b87065d61f7c95
Will Enright <wenright@purdue.edu>
2016-05-27 23:17:44 -0400

    Switch player from graphics translation to physical vertex transformation

--- scripts/player.lua
+++ scripts/player.lua
@@ -1,24 +1,30 @@
 local Player = Class {
 	__includes = Transform,
+	rotationSpeed = 4,
+	acceleration = 6,
 	verts = {
 		0,-25,
 		20, 25,
 		0, 10,
 		-20, 25
-	},
-	rotationSpeed = 4,
-	acceleration = 6
+	}
 }
 
 function Player:init()
 	Transform.init(self, 0, 0, 40, 50)
 
-	self.r = -math.pi/2
+	self.worldVerts = Class.clone(self.verts)
+
+	self.r = 0
 end
 
 function Player:update(dt)
+	--------------------------------------------------
+	-- Update the position using the player's velocity
 	Transform.update(self, dt)
 
+	-----------------------------------------------------------
+	-- Take input from keyboard and apply a rotation or a force
 	if love.keyboard.isDown('a', 'left') then
 		self.r = self.r - dt * self.rotationSpeed
 	end
@@ -27,28 +33,39 @@ function Player:update(dt)
 	end
 
 	if love.keyboard.isDown('w', 'up') then
-		self:applyForce(math.cos(self.r) * self.acceleration * dt, math.sin(self.r) * self.acceleration * dt)
+		self:applyForce(math.cos(self.r - math.pi/2) * self.acceleration * dt, math.sin(self.r - math.pi/2) * self.acceleration * dt)
+	end
+
+	------------------------------------------------------------
+	-- Update vertices from template into real world coordinates
+
+	-- Just some helpers for trig functions
+	local cos, sin = math.cos(self.r), math.sin(self.r)
+
+	-- Rotate vertices
+	for i=1, #self.worldVerts, 2 do
+		self.worldVerts[i] = self.verts[i] * cos - self.verts[i + 1] * sin
+		self.worldVerts[i + 1] = self.verts[i] * sin + self.verts[i + 1] * cos
+	end
+
+	-- Translate vertices
+	for i=1, #self.worldVerts, 2 do
+		self.worldVerts[i] = self.worldVerts[i] + self.x
+		self.worldVerts[i + 1] = self.worldVerts[i + 1] + self.y
 	end
 end
 
 function Player:draw()
-	love.graphics.push()
-
-	love.graphics.translate(self.x, self.y)
-	love.graphics.rotate(self.r + math.pi / 2)
-
 	love.graphics.setColor(0, 0, 0)
-	love.graphics.polygon('fill', self.verts)
+	love.graphics.polygon('fill', self.worldVerts)
 
 	love.graphics.setColor(255, 255, 255)
 
 	-- Player
-	love.graphics.polygon('line', self.verts)
+	love.graphics.polygon('line', self.worldVerts)
 
 	-- Paddle
 	love.graphics.rectangle('fill', -25, -40, 50, 5, 2)
-
-	love.graphics.pop()
 end
 
 return Player
