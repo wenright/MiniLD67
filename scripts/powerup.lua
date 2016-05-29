8544ccfac0a72c72720b49498eebb3884f203ed5
Will Enright <wenright@purdue.edu>
2016-05-28 22:50:25 -0400

    Add mayhem

--- scripts/powerup.lua
+++ scripts/powerup.lua
@@ -9,12 +9,15 @@ function Powerup:init(x, y)
 	-- TODO pick random powerup type
 	self.kind = love.math.random(1, 1)
 
+	self.color = {255, 255, 255, 255}
+
 	self.radius = 1
+
 	Timer.tween(0.4, self, {radius = 50}, 'in-out-quad')
 end
 
 function Powerup:collide()
-	if dist(Game.player.x, Game.player.y, self.x, self.y) < 50 then
+	if not self.used and dist(Game.player.x, Game.player.y, self.x, self.y) < 100 then
 		if self.kind == 1 then
 			Instantiate(Ball(self.x, self.y, love.math.random() * math.pi * 4 - math.pi * 2))
 			Instantiate(Ball(self.x, self.y, love.math.random() * math.pi * 4 - math.pi * 2))
@@ -22,14 +25,17 @@ function Powerup:collide()
 			-- Score bonus? bigger paddle?
 		end
 
-		print('Got a powerup!')
-		Game.objects:remove(self)
+		self.used = true
+		Timer.tween(0.2, self, {color = {255, 255, 255, 0}})
+		Timer.tween(0.2, self, {radius = 100}, 'in-quad', function()
+			Game.objects:remove(self)
+		end)
 	end
 end
 
 function Powerup:draw()
 	-- TODO flashy animations and stuff
-	love.graphics.setColor(255, 255, 255)
+	love.graphics.setColor(self.color)
 	love.graphics.circle('line', self.x, self.y, self.radius)
 end
 
